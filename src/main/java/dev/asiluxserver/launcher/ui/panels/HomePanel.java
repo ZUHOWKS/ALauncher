621b478dd50c7228e2dbb48de3cb1608f8e7dac8
oem <jojo.vilard38@gmail.com>
2021-09-29 20:10:10 +0200

    ALauncher Base

--- src/main/java/dev/asiluxserver/launcher/ui/panels/HomePanel.java
+++ src/main/java/dev/asiluxserver/launcher/ui/panels/HomePanel.java
@@ -3,6 +3,7 @@ package dev.asiluxserver.launcher.ui.panels;
 import dev.asiluxserver.launcher.ui.PanelManager;
 import dev.asiluxserver.launcher.ui.panel.Panel;
 import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
@@ -22,6 +23,10 @@ public class HomePanel extends Panel {
         GridPane leftBarPanel = new GridPane();
         setGrow(leftBarPanel);
         setGrow(leftBarPanel);
+        setAlignment(leftBarPanel, HPos.LEFT, VPos.CENTER);
+        leftBarPanel.setMinWidth(100);
+        leftBarPanel.setMaxWidth(100);
+        leftBarPanel.setStyle("-fx-background-color: rgba(24,24,24,1);");
 
         this.layout.add(leftBarPanel, 0 , 0);
         this.layout.add(centerPane, 1, 0);
