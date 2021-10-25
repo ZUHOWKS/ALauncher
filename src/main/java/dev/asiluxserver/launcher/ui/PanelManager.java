a2c4ae7255cfd9349a2d06fb633f075277ff90f8
ZUHOWKS <jojo.vilard38@gmail.com>
2021-10-25 18:44:48 +0200

    PanelManager -> Modification taille de la fenêtre.

--- src/main/java/dev/asiluxserver/launcher/ui/PanelManager.java
+++ src/main/java/dev/asiluxserver/launcher/ui/PanelManager.java
@@ -28,10 +28,10 @@ public class PanelManager {
 
     public void init() {
         this.stage.setTitle("Asilux");
-        this.stage.setMinWidth(1280);
-        this.stage.setMinHeight(720);
-        this.stage.setWidth(1280);
-        this.stage.setHeight(720);
+        this.stage.setMinWidth(1180);
+        this.stage.setMinHeight(620);
+        this.stage.setWidth(1180);
+        this.stage.setHeight(620);
         this.stage.centerOnScreen();
         this.stage.getIcons().add(new Image("images/asilux-icone.png"));
 
