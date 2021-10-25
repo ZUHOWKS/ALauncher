a233b44d8e2c8f187bc2dae49abf2564a7520a04
Warna <ndupont2810@gmail.com>
2021-10-25 19:08:59 +0200

    Added main function.

--- src/main/java/dev/asiluxserver/launcher/Main.java
+++ src/main/java/dev/asiluxserver/launcher/Main.java
@@ -1,4 +1,24 @@
 package dev.asiluxserver.launcher;
 
 
-public class Main {}
+import javafx.application.Application;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("javafx.application.Application");
+            Application.launch(Launcher.class, args);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Erreur:\n" + e.getMessage() + " not found",
+                    "Erreur",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
+}
