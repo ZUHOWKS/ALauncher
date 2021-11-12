5abb2dc1dfce1b44bc3849a79428f9107c9e3d95
ZUHOWKS <jojo.vilard38@gmail.com>
2021-11-12 23:35:55 +0100

    Classe `XMLPatchParser` terminé | On peut lire un fichier `.xml` à partir d'un URL via `DOM`.

--- src/main/java/dev/asiluxserver/launcher/utils/XML/XMLPatchParser.java
+++ src/main/java/dev/asiluxserver/launcher/utils/XML/XMLPatchParser.java
@@ -1,40 +1,61 @@
 package dev.asiluxserver.launcher.utils.XML;
 
+import dev.asiluxserver.launcher.utils.patch.PatchMessage;
+import dev.asiluxserver.launcher.utils.patch.PatchNote;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.sound.midi.Patch;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class XMLPatchParser {
+
+    String url;
+    PatchMessage patchMessage;
+
     public XMLPatchParser(String url) {
+        this.url = url;
+    }
+
+    public PatchMessage getPatch() {
+        readEvent();
+        return patchMessage;
+    }
+
+    public void readEvent() {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
-            Document document = db.parse(new URL(url).openStream());
+            Document document = db.parse(new URL(this.url).openStream());
             document.getDocumentElement().normalize();
-            System.out.println("Root Element :" + document.getDocumentElement().getAttribute("id"));
+            this.patchMessage.setTitle(document.getDocumentElement().getAttribute("id"));
             NodeList nList = document.getElementsByTagName("categories");
-            System.out.println("----------------------------");
+
             for (int temp = 0; temp < nList.getLength(); temp++) {
+
                 Node nNode = nList.item(temp);
-                System.out.println("");
+
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
                     Element eElement = (Element) nNode;
                     NodeList noteListe = ((Element) nNode).getElementsByTagName("note");
-                    System.out.println(eElement.getAttribute("id").toUpperCase(Locale.ROOT));
+                    PatchNote patchNote = new PatchNote(eElement.getAttribute("id").toUpperCase(Locale.ROOT));
+
                     for (int j = 0; j < noteListe.getLength(); j++) {
-                        Node noteNode = (Element) noteListe.item(j);
-                        System.out.println("   - " + noteNode.getTextContent());
+                        Node noteNode = noteListe.item(j);
+                        patchNote.addNote(noteNode.getTextContent());
                     }
+                    this.patchMessage.addPatchNote(patchNote);
                 }
             }
         }
