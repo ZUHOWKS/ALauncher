c303e5fdf3880af750304912c70953e66e42c16c
ZUHOWKS <jojo.vilard38@gmail.com>
2021-11-17 07:00:34 +0100

    Project File

--- src/main/java/dev/asiluxserver/launcher/utils/patch/PatchLoader.java
+++ src/main/java/dev/asiluxserver/launcher/utils/patch/PatchLoader.java
@@ -1,13 +1,22 @@
 package dev.asiluxserver.launcher.utils.patch;
 
 import dev.asiluxserver.launcher.utils.XML.XMLPatchParser;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class PatchLoader  {
 
     XMLPatchParser patchFile;
+    ArrayList<ArrayList<Label>> patchNoteLabel = new ArrayList<>();
     Label titleLabel = new Label("");
     Label patchLabel = new Label("");
 
@@ -22,15 +31,48 @@ public class PatchLoader  {
         this.titleLabel.setText(patchMessage.getTitle());
 
         for (int i = 0; i < patchNotes.size(); i++) {
+
             PatchNote patchNote = patchNotes.get(i);
             ArrayList<String> notes = patchNote.getNote();
             patchLabel.setText(patchLabel.getText() + (!patchLabel.getText().equals("") ? "\n\n" : "") +
                     patchNote.getCategories() + "\n");
-            for (int j = 0; j < patchNote.getNote().size(); j++) {
+
+            ArrayList<Label> labels = new ArrayList<>();
+
+            Label categoriesLabel = new Label(patchNote.getCategories().toUpperCase(Locale.ROOT));
+            categoriesLabel.setAlignment(Pos.TOP_LEFT);
+            GridPane.setHalignment(categoriesLabel, HPos.LEFT);
+            GridPane.setValignment(categoriesLabel, VPos.TOP);
+            GridPane.setHgrow(categoriesLabel, Priority.ALWAYS);
+            GridPane.setVgrow(categoriesLabel, Priority.ALWAYS);
+            categoriesLabel.setStyle("-fx-text-size: 32; -fx-text-fill: rgb(0, 0, 0);");
+            categoriesLabel.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 35));
+            categoriesLabel.setTranslateX(50);
+
+            Label noteLabel = new Label();
+            noteLabel.setAlignment(Pos.TOP_LEFT);
+            GridPane.setHalignment(noteLabel, HPos.LEFT);
+            GridPane.setValignment(noteLabel, VPos.TOP);
+            GridPane.setHgrow(noteLabel, Priority.ALWAYS);
+            GridPane.setVgrow(noteLabel, Priority.ALWAYS);
+            noteLabel.setStyle("-fx-text-size: 25; -fx-text-fill: rgb(0, 0, 0);");
+            noteLabel.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 25));
+            noteLabel.setTranslateX(50);
+            noteLabel.setTranslateY(50);
+
+            int noteSize = notes.size();
+            for (int j = 0; j < noteSize; j++) {
                 patchLabel.setText(patchLabel.getText() + "\n" +
                         "  - " + notes.get(j)
                 );
+                noteLabel.setText(noteLabel.getText() + "  - " + notes.get(j) + "\n");
             }
+
+            labels.add(categoriesLabel);
+            labels.add(noteLabel);
+
+            patchNoteLabel.add(labels);
+
         }
     }
 
@@ -50,4 +92,7 @@ public class PatchLoader  {
         return this.patchFile;
     }
 
+    public ArrayList<ArrayList<Label>> getPatchNoteLabel() {
+        return patchNoteLabel;
+    }
 }
