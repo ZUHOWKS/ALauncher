5b1ad021fbf5135e6b9f79689fecd20c65ff841c
ZUHOWKS <jojo.vilard38@gmail.com>
2021-11-20 11:03:33 +0100

    Ajout de couleurs.

--- src/main/java/dev/asiluxserver/launcher/ui/assets/Colors.java
+++ src/main/java/dev/asiluxserver/launcher/ui/assets/Colors.java
@@ -19,16 +19,19 @@ public enum Colors {
     DARK_GREY_3(Color.rgb(37, 37, 37, 0.3)),
     DARK_GREY_4(Color.rgb(37, 37, 37, 0.2)),
     DARK_GREY_5(Color.rgb(37, 37, 37, 0.17)),
+    DARK_GREY_6(Color.rgb(47, 47, 47)),
+    DARK_GREY_7(Color.rgb(67, 67, 67)),
 
     BLACK_1(Color.rgb(27,27,27,0.95)),
     BLACK_2(Color.rgb(27,27,27,0.3)),
 
     LIGHT_GREEN_1(Color.rgb(245, 255, 245)),
-    LIGHT_GREEN_2(Color.rgb(100, 142, 56)),
+    LIGHT_GREEN_2(Color.rgb(97, 136, 56)),
     LIGHT_GREEN_3(Color.rgb(143, 168, 95)),
     LIGHT_GREEN_4(Color.rgb(155, 190, 82)),
     LIGHT_GREEN_5(Color.rgb(165, 210, 72)),
-    LIGHT_GREEN_6(Color.rgb(167, 220, 82)),
+    LIGHT_GREEN_6(Color.rgb(177, 228, 82)),
+    LIGHT_GREEN_7(Color.rgb(167, 220, 82, 0.5)),
 
     RED_1(Color.rgb(225,53,53)),
     ;
